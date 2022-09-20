e6ee924a2d171f24295671d7fe3baf6acc2e3ab5
Guilherme Gomes <35308442+Guilherme09396@users.noreply.github.com>
2022-09-19 22:17:25 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,9 +2,9 @@ var knex = require('knex')({
     client: 'mysql2',
     connection: {
       host : '127.0.0.1',
-      user : 'admin',
-      password : 'P@ssw0rd',
-      database : 'banco_users'
+      user : '',
+      password : '',
+      database : ''
     }
   });
 
